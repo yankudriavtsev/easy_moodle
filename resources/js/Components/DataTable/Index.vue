7558ff4dbffbb71d69690cb1a8293958fdbb2439
Yan Kudriavtsev <yan@intelliboard.net>
2022-05-07 11:23:53 +0300

    added pnaled for DataTable JS component

--- resources/js/Components/DataTable/Index.vue
+++ resources/js/Components/DataTable/Index.vue
@@ -1,36 +1,48 @@
 <template>
-    <table class="mb-3 w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-            <th class="px-6 py-3" v-for="column in columns">
-                {{ column.title }}
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            v-for="(row, index) in items.data"
-            :key="'tbl_row' + index"
-        >
-            <template v-for="column in columns">
-                <td class="px-6 py-4">
-                    <template v-if="column.component">
-                        <component :is="column.component" v-bind="column.getProps(row)" />
-                    </template>
-                    <template v-else>
-                        {{ row[column.key] }}
-                    </template>
-                </td>
-            </template>
-        </tr>
-        </tbody>
-    </table>
-    <pagination :links="items.links"></pagination>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <panel class="p-4" :configuration="panelConfiguration" />
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th class="px-6 py-3" v-for="column in columns">
+                        {{ column.title }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-if="!items.data.length">
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td :colspan="columns.length" class="px-6 py-4 text-center text-lg">
+                            No records
+                        </td>
+                    </tr>
+                </template>
+
+                <template v-else>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                        v-for="(row, index) in items.data"
+                        :key="'tbl_row' + index"
+                    >
+                        <td v-for="column in columns" class="px-6 py-4">
+                            <template v-if="column.component">
+                                <component :is="column.component" v-bind="column.getProps(row)" />
+                            </template>
+                            <template v-else>
+                                {{ row[column.key] }}
+                            </template>
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+    </div>
+
+    <pagination class="mt-3" :links="items.links"></pagination>
 </template>
 
 <script>
     import Pagination from "@/Components/DataTable/Pagination";
-    import Actions from "@/Pages/Instance/Components/TableRowActions";
+    import Panel from "./Panel";
 
     export default {
         props: {
@@ -41,11 +53,15 @@
             items: {
                 type: Object,
                 required: true
-            }
+            },
+            panelConfiguration: {
+                type: Object,
+                default: () => ({})
+            },
         },
         components: {
-            Actions,
             Pagination,
+            Panel,
         },
     };
 </script>
