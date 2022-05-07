cc670116550632356968bfa0612a4893378b4a3e
Yan Kudriavtsev <yan@intelliboard.net>
2022-05-07 11:25:11 +0300

    added .btn mixin

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,5 +1,6 @@
 <script setup>
 defineProps({
+    // TODO check for non=submit button
     type: {
         type: String,
         default: 'submit',
@@ -8,7 +9,7 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button :type="type" class="btn">
         <slot />
     </button>
 </template>
